a17946e7a135abd16e7aaecd4240007222175eff
Rob Rudin <rjrudin@gmail.com>
2015-07-21 21:20:37 -0400

    #16 Now asserting on the messages that are generated via script elements

--- src/test/java/com/marklogic/scxml/transition/TransitionAcrossChildStatesTest.java
+++ src/test/java/com/marklogic/scxml/transition/TransitionAcrossChildStatesTest.java
@@ -7,24 +7,31 @@ import com.marklogic.scxml.Instance;
 
 public class TransitionAcrossChildStatesTest extends AbstractScxmlTest {
 
+    private Instance instance;
+
     @Test
     public void test() {
         String instanceId = startMachineWithId("spec-3.1.5");
 
-        Instance i = loadInstance(instanceId);
-        i.assertActiveStates("S", "s1", "s11");
+        instance = loadInstance(instanceId);
+        instance.assertActiveStates("S", "s1", "s11");
+        instance.assertElementMissing("No messages should have been logged yet", "//message");
 
-        /**
-         * The "e" transition is on state s1. s11 doesn't have any transitions, but we still leave it because we're
-         * existing s1, which means we also need to fire its s11's onExit block.
-         */
         fireEvent(instanceId, "e");
 
-        i = loadInstance(instanceId);
-        i.assertActiveStates("S", "s2", "s21");
-        i.assertTransitionExists(2, "e", "s1", "s2");
-        i.assertTransitionExists(2, "e", "s1", "s21");
-        
-        i.prettyPrint();
+        instance = loadInstance(instanceId);
+        instance.assertActiveStates("S", "s2", "s21");
+        instance.assertTransitionExists(2, "e", "s1", "s2");
+        instance.assertTransitionExists(2, "e", "s1", "s21");
+        assertMessageExists(1, "leaving s11");
+        assertMessageExists(2, "leaving s1");
+        assertMessageExists(3, "executing e transition");
+        assertMessageExists(4, "entering s2");
+        assertMessageExists(5, "entering s21");
+        instance.prettyPrint();
+    }
+
+    private void assertMessageExists(int position, String message) {
+        instance.assertElementExists(format("/mlsc:instance/message[%d][. = '%s']", position, message));
     }
 }
